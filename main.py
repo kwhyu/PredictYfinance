84245b82605de3e59acf577ba62b9887073726d3
Eka Wahyu <94496984+kwhyu@users.noreply.github.com>
2024-07-09 19:15:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import seaborn as sns
 from datetime import datetime
 
+import subprocess
+import sys
+
+subprocess.check_call([sys.executable, "-m", "pip", "install", "yfinance", "streamlit"])
+
 # Load stock data
 @st.cache_data
 def load_stock_data(ticker, period="5y"):
